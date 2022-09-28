d93343e805e4e717c648660d2e71985b35dfa97e
Aventure20 <aventure.dev.code@gmail.com>
2022-09-28 17:12:45 +0200

    Multiple films adding
    I added the possibility to add multiples films without having to restart the entire script.

--- main.py
+++ main.py
@@ -2,31 +2,51 @@ import random
 from os.path import exists
 import sys
 
+def writefilm(title):
+    print("Ok! Adding {} to your films list.".format(title))
+    with open("filmslist.txt", "a+") as file:
+        file.write("{}\n".format(title))
+        file.close()
+
+def addfilms():
+    film_title = input("Ok! What is the title of the film you want to add?\n")
+    verification = input("Do you want to add {} to your films list?\n".format(film_title)).lower()
+    while True:
+        if verification == "yes":
+            writefilm(film_title)
+            more = input("Do you want to add another film?\n").lower()
+            if more == "yes":
+                addfilms()
+                break
+            elif more == "no":
+                sys.exit()
+            else:
+                print("Please awnser by Yes or No.")
+                more = input("Do you want to add another film?\n").lower()
+                continue
+            break
+        
+        elif verification == "no":
+            film_title = input("Ok! What is the title of the film you want to add?\n")
+            verification = input("Do you want to add {} to your films list?\n".format(film_title)).lower()
+            continue
+
+        else:
+            print("Please awnser by Yes or No.")
+            verification = input("Do you want to add {} to your films list?\n".format(film_title)).lower()
+            continue
+
+
 print("Hi, Welcome to WhatToWatch!")
 print("Please select one of the options above:")
 print("1: Add a film")
 print("2: Choose a film for me")
+
 main = input("So! What do you want to do?\n")
+
 while True:
     if main == "1":
-        film_title = input("Ok! What is the title of the film you want to add?\n")
-        verification = input("Do you want to add {} to your films list?\n".format(film_title)).lower()
-        while True:
-            if verification == "yes":
-                print("Ok! Adding {} to your films list.".format(film_title))
-                with open("filmslist.txt", "a+") as file:
-                    file.write("{}\n".format(film_title))
-                    file.close()
-                break
-            elif verification == "no":
-                film_title = input("Ok! What is the title of the film you want to add?\n")
-                verification = input("Do you want to add {} to your films list?\n".format(film_title)).lower()
-                continue
-
-            else:
-                print("Please awnser by Yes or No.")
-                verification = input("Do you want to add {} to your films list?\n".format(film_title)).lower()
-                continue
+        addfilms()
         break
     
     elif main == "2":
@@ -47,6 +67,7 @@ while True:
         print(films_list)
         print(random.choice(films_list))
         break
+
     else:
         print("Sorry, I don't understand. Please awnser with the numbers only.")
         main = input("So! What do you want to do?\n")
